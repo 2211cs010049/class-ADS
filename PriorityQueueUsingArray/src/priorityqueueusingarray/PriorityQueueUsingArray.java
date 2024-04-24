9ba2a3c6dc73c43cb6a2c293f1e3bd92dc6df9a5
2211cs010049 <158037364+2211cs010049@users.noreply.github.com>
2024-04-24 10:29:50 +0530

    Add files via upload

---
+++ PriorityQueueUsingArray/src/priorityqueueusingarray/PriorityQueueUsingArray.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package priorityqueueusingarray;
+public class PriorityQueueUsingArray {
+ public static void main(String[] args) {
+ PriorityQueue queue = new PriorityQueue();
+ queue.add(5);
+ queue.add(3);
+ queue.add(6);
+ queue.add(1);
+ queue.add(4);
+ System.out.println(queue);
+ }
+}
